93839168e0ececed771017ce48b5d36cafeb5277
drdani <drdani@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2000-01-31 08:02:25 +0000

    Delete Makefile.common (generated file) drdani
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@42 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- clean.mk
+++ clean.mk
@@ -9,7 +9,7 @@ clean:
 # Deleting all files created by configuring or building the program
 # -----------------------------------------------------------------
 distclean: clean
-	rm -f config.cache config.log config.status
+	rm -f config.cache config.log config.status Makefile.common
 	rm -f sdccconf.h main.mk *.dep
 
 
