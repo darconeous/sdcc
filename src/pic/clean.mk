778f1b919b8f1bc96249966828c4b7b209d1b212
sdattalo <sdattalo@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2000-12-04 06:21:46 +0000

    Started PIC port (based on mcs51 port)
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@512 4a8a32a2-be11-0410-ad9d-d568d2c75423

---
+++ src/pic/clean.mk
@@ -0,0 +1,6 @@
+#
+
+clean:
+	rm -f $(LIB) *.o *~ port.a
+
+# End of src/mcs51/clean.mk
---
+++ src/pic/main.c
@@ -0,0 +1,247 @@
+/** @file main.c
+    pic14 specific general functions.
+
+    Note that mlh prepended _pic14_ on the static functions.  Makes
+    it easier to set a breakpoint using the debugger.
+*/
+#include "common.h"
+#include "main.h"
+#include "ralloc.h"
+#include "gen.h"
+
+static char _defaultRules[] =
+{
+#include "peeph.rul"
+};
+
+/* list of key words used by pic14 */
+static char *_pic14_keywords[] =     {
+    "at",
+    "bit",
+    "code",
+    "critical",
+    "data",
+    "far",
+    "idata",
+    "interrupt",
+    "near",
+    "pdata",
+    "reentrant",
+    "sfr",
+    "sbit",
+    "using",
+    "xdata",
+    "_data",
+    "_code",
+    "_generic",
+    "_near",
+    "_xdata",
+    "_pdata",
+    "_idata",
+    NULL
+};
+
+
+void pic14_assignRegisters (eBBlock **ebbs, int count);
+
+static int regParmFlg = 0; /* determine if we can register a parameter */
+
+static void _pic14_reset_regparm()
+{
+    regParmFlg = 0;
+}
+
+static int _pic14_regparm( link *l)
+{
+    /* for this processor it is simple
+       can pass only the first parameter in a register */
+    if (regParmFlg)
+	return 0;
+
+    regParmFlg = 1;
+    return 1;
+}
+
+static bool _pic14_parseOptions(int *pargc, char **argv, int *i)
+{
+    /* TODO: allow port-specific command line options to specify
+     * segment names here.
+     */
+    return FALSE;
+}
+
+static void _pic14_finaliseOptions(void)
+{
+    /* Hack-o-matic: if we are using the flat24 model,
+     * adjust pointer sizes.
+     */
+    if (options.model == MODEL_FLAT24)
+    {
+        port->s.fptr_size = 3;
+        port->s.gptr_size = 4;
+        port->stack.isr_overhead++;   /* Will save dpx on ISR entry. */
+        #if 1
+        port->stack.call_overhead++; 	   /* This acounts for the extra byte 
+        				    * of return addres on the stack.
+        				    * but is ugly. There must be a 
+        				    * better way.
+        				    */
+	#endif
+        fReturn = fReturn390;
+        fReturnSize = 5;
+    } 
+
+    if (options.model) {
+	port->mem.default_local_map = xdata;
+	port->mem.default_globl_map = xdata;
+    } else {
+	port->mem.default_local_map = data;
+	port->mem.default_globl_map = data;
+    }
+    
+    if (options.stack10bit)
+    {
+    	if (options.model != MODEL_FLAT24)
+    	{
+            fprintf(stderr, 
+        	    "*** warning: 10 bit stack mode is only supported in flat24 model.\n");
+            fprintf(stderr, "\t10 bit stack mode disabled.\n");
+            options.stack10bit = 0;
+        }
+        else
+        {
+            /* Fixup the memory map for the stack; it is now in
+             * far space and requires a FPOINTER to access it.
+             */
+            istack->fmap = 1;
+            istack->ptrType = FPOINTER; 
+        }
+    }
+}
+
+static void _pic14_setDefaultOptions(void)
+{
+}
+
+static void _pic14_init(void)
+{
+  printf("cool init was called for pic14\n");
+
+}
+static const char *_pic14_getRegName(struct regs *reg)
+{
+    if (reg)
+	return reg->name;
+    return "err";
+}
+
+static void _pic14_genAssemblerPreamble(FILE *of)
+{
+  fprintf(of,"\tlist\tp=16c84\n");
+  fprintf(of,"\t__config _wdt_off\n");
+  fprintf(of,"\ninclude \"p16c84.inc\"\n");
+
+}
+
+/* Generate interrupt vector table. */
+static int _pic14_genIVT(FILE *of, symbol **interrupts, int maxInterrupts)
+{
+    int i;
+    
+    if (options.model != MODEL_FLAT24)
+    {
+        /* Let the default code handle it. */
+    	return FALSE;
+    }
+    
+    fprintf (of, ";\tajmp\t__sdcc_gsinit_startup\n");
+    
+    /* now for the other interrupts */
+    for (i = 0; i < maxInterrupts; i++) 
+    {
+	if (interrupts[i])
+	{
+	    fprintf(of, ";\tljmp\t%s\n;\t.ds\t4\n", interrupts[i]->rname);
+	}
+	else
+	{
+	    fprintf(of, ";\treti\n;\t.ds\t7\n");
+	}
+    }
+    
+    return TRUE;
+}
+
+/** $1 is always the basename.
+    $2 is always the output file.
+    $3 varies
+    $l is the list of extra options that should be there somewhere...
+    MUST be terminated with a NULL.
+*/
+static const char *_linkCmd[] = {
+    "aslink", "-nf", "$1", NULL
+};
+
+static const char *_asmCmd[] = {
+    "asxpic14", "-plosgffc", "$1.asm", NULL
+};
+
+/* Globals */
+PORT pic14_port = {
+    "pic14",
+    "MCU pic14",			/* Target name */
+    {
+	TRUE,			/* Emit glue around main */
+    },
+    {	
+	_asmCmd,
+	"-plosgffc",		/* Options with debug */
+	"-plosgff",		/* Options without debug */
+    },
+    {
+	_linkCmd
+    },
+    {
+	_defaultRules
+    },
+    {
+    	/* Sizes: char, short, int, long, ptr, fptr, gptr, bit, float, max */
+	1, 1, 2, 4, 1, 2, 3, 1, 4, 4
+    },
+    {
+	"XSEG    (XDATA)",
+	"STACK   (DATA)",
+	"CSEG    (CODE)",
+	"DSEG    (DATA)",
+	"ISEG    (DATA)",
+	"XSEG    (XDATA)",
+	"BSEG    (BIT)",
+	"RSEG    (DATA)",
+	"GSINIT  (CODE)",
+	"OSEG    (OVR,DATA)",
+	"GSFINAL (CODE)",
+	NULL,
+	NULL,
+	1
+    },
+    { 
+	+1, 1, 4, 1, 1
+    },
+    /* pic14 does not have an 8 bit mul */
+    {
+	0
+    },
+    _pic14_init,
+    _pic14_parseOptions,
+    _pic14_finaliseOptions,
+    _pic14_setDefaultOptions,
+    pic14_assignRegisters,
+    _pic14_getRegName ,
+    _pic14_keywords,
+    _pic14_genAssemblerPreamble,
+    _pic14_genIVT ,
+    _pic14_reset_regparm,
+    _pic14_regparm,
+    NULL
+};
+
