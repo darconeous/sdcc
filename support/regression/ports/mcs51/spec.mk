783c03615f9c0161d6eec6dd8c4585587e412140
bernhardheld <bernhardheld@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2001-08-23 12:59:13 +0000

    add mcs51-port
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@1164 4a8a32a2-be11-0410-ad9d-d568d2c75423

---
+++ support/regression/ports/mcs51/spec.mk
@@ -0,0 +1,32 @@
+# Port specification for the mcs51 port running with uCsim
+
+S51 = ../../sim/ucsim/s51.src/s51
+
+SDCCFLAGS += --lesspedantic -DREENTRANT=reentrant -DGENERIC=_generic
+
+OBJEXT = .rel
+EXEEXT = .ihx
+
+# Needs parts of gbdk-lib, namely the internal mul/div/mod functions.
+EXTRAS = fwk/lib/testfwk$(OBJEXT) ports/$(PORT)/support$(OBJEXT)
+
+# Rule to link into .ihx
+%.ihx: %$(OBJEXT) $(EXTRAS)
+	$(SDCC) $(SDCCFLAGS) $(EXTRAS) $<
+	mv fwk/lib/testfwk.ihx $@
+	mv fwk/lib/testfwk.map $(@:.ihx=.map)
+
+# %$(OBJEXT): %.asm
+#	../../bin/as-z80 -plosgff $@ $<
+
+# %$(OBJEXT): %.s
+#	../../bin/as-z80 -plosgff $@ $<
+
+%$(OBJEXT): %.c
+	$(SDCC) $(SDCCFLAGS) -c $<
+
+# PENDING: Path to sdcc-extra
+%.out: %$(EXEEXT)
+	mkdir -p `dirname $@`
+	$(S51) -t32 -S in=$(shell tty),out=$@ $< < ports/mcs51/uCsim.cmd >/dev/null 2>&1
+	-grep -n FAIL $@ /dev/null || true
