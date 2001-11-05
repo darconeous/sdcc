f990f58fc49fae4c5a24d18f1206cc37bdbf1ccc
michaelh <michaelh@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2001-11-05 04:26:36 +0000

    Added heap support functions
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@1505 4a8a32a2-be11-0410-ad9d-d568d2c75423

---
+++ device/lib/gbz80/heap.s
@@ -0,0 +1,14 @@
+        ;; Stubs that hook the heap in
+        .globl  __sdcc_heap_init
+        
+        .area   _GSINIT
+        call    __sdcc_heap_init
+        
+        .area   _HEAP
+__sdcc_heap_start::
+        ;; For now just allocate 1k of heap.
+        .ds     1023
+        
+        .area   _HEAP_END
+__sdcc_heap_end::
+        .ds     1
