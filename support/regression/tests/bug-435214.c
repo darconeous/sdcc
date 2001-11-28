fd76df2157efa9dab74c9597b665c6117c9b468c
michaelh <michaelh@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2001-11-28 01:58:43 +0000

    Fixed up extra static
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@1651 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/regression/tests/bug-435214.c
+++ support/regression/tests/bug-435214.c
@@ -2,14 +2,13 @@
  */
 #include <testfwk.h>
 
-static 
 unsigned long divide(long a)
 {
     return a/512ul;
 }
 
-static void
-testDivide()
+void
+testDivide(void)
 {
   ASSERT(divide(1300) == 2);
   ASSERT(divide(0x12345678) == 0x91A2B);
