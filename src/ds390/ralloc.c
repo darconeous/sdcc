0ad57338fe62618aafe7a81929c36d5eb931372d
sandeep <sandeep@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2001-12-24 22:40:00 +0000

    Had broken the regression testsuite
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@1742 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- src/ds390/ralloc.c
+++ src/ds390/ralloc.c
@@ -2137,7 +2137,10 @@ packRegsDPTRuse (operand * op)
 	    if (OP_SYMBOL(IC_RESULT(ic))->liveTo == 
 		OP_SYMBOL(IC_RESULT(ic))->liveFrom) continue ;
 	    etype = getSpec(type = operandType(IC_RESULT(ic)));
+#if 0
 	    if (getSize(type) == 0 || isOperandEqual(op,IC_RESULT(ic))) 
+#endif
+	    if (getSize(type) == 0)
 		continue ;
 	    return NULL ;
 	}
