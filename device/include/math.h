1071a6c6ae98b2f1017cc0bc323860ac0d145d8c
johanknol <johanknol@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2002-01-11 09:17:33 +0000

    added math libs of Jesus Calvino-Fraga
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@1787 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- device/include/math.h
+++ device/include/math.h
@@ -1,33 +1,79 @@
-/*-------------------------------------------------------------------------
-  math.h - ANSI functions forward declarations
- 
-             Written By -  Sandeep Dutta . sandeep.dutta@usa.net (1998)
+/*  math.h: Floating point math function declarations
 
-   This program is free software; you can redistribute it and/or modify it
-   under the terms of the GNU General Public License as published by the
-   Free Software Foundation; either version 2, or (at your option) any
-   later version.
-   
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-   
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-   
-   In other words, you are welcome to use, share and improve this program.
-   You are forbidden to forbid anyone else to use, share and improve
-   what you give them.   Help stamp out software-hoarding!  
--------------------------------------------------------------------------*/
+    Copyright (C) 2001  Jesus Calvino-Fraga, jesusc@ieee.org 
 
-#ifndef __SDC51_MATH_H
-#define __SDC51_MATH_H 1
-#error Floating point not yet completely implemented
-#endif
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
 
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
 
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA */
 
+/* Version 1.0 - Initial release */
 
+#ifndef _INC_MATH
+#define _INC_MATH
 
+#define PI          3.1415926536
+#define TWO_PI      6.2831853071
+#define HALF_PI     1.5707963268
+#define QUART_PI    0.7853981634
+#define iPI         0.3183098862
+#define iTWO_PI     0.1591549431
+#define TWO_O_PI    0.6366197724
+
+// EPS=B**(-t/2), where B is the radix of the floating-point representation
+// and there are t base-B digits in the significand.  Therefore, for floats
+// EPS=2**(-12).  Also define EPS2=EPS*EPS.
+#define EPS 244.14062E-6
+#define EPS2 59.6046E-9
+#define XMAX 3.402823466E+38
+
+union float_long
+{
+    float f;
+    long l;
+};
+
+/**********************************************
+ * Prototypes for float ANSI C math functions *
+ **********************************************/
+
+/* Trigonometric functions */
+float sinf(const float x);
+float cosf(const float x);
+float tanf(const float x);
+float cotf(const float x);
+float asinf(const float x);
+float acosf(const float x);
+float atanf(const float x);
+float atan2f(const float x, const float y);
+
+/* Hyperbolic functions */
+float sinhf(const float x);
+float coshf(const float x);
+float tanhf(const float x);
+
+/* Exponential, logarithmic and power functions */
+float expf(const float x);
+float logf(const float x);
+float log10f(const float x);
+float powf(const float x, const float y);
+float sqrtf(const float a);
+
+/* Nearest integer, absolute value, and remainder functions */
+float fabsf(const float x);
+float frexpf(const float x, int *pw2);
+float ldexpf(const float x, const int pw2);
+float ceilf(float x);
+float floorf(float x);
+float modff(float x, float * y);
+
+#endif  /* _INC_MATH */
