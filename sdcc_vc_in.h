1032834fbc3a0d274d0b84f54028ab98860579a2
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2002-08-28 11:45:20 +0000

    added - genarate sdcc_vc.h using sdcc_vc_in.h as template and insert the version number definitions from .version
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@2082 4a8a32a2-be11-0410-ad9d-d568d2c75423

---
+++ sdcc_vc_in.h
@@ -0,0 +1,48 @@
+#ifndef SDCC_VC_HEADER
+#define SDCC_VC_HEADER
+
+
+#undef SDCC_VERSION_HI
+#undef SDCC_VERSION_LO
+#undef SDCC_VERSION_P
+#undef SDCC_VERSION_STR
+
+/* MSVC 6 does not have __FUNCTION__ preprocessor macro defined */
+
+#if defined(_MSC_VER) && (_MSC_VER < 1300)
+#define __FUNCTION__ __FILE__
+#endif
+
+#undef PREFIX
+#undef DATADIR
+#undef SRCDIR
+
+#define PREFIX                "\\sdcc"
+
+//#define STANDARD_INCLUDE_DIR  "\\sdcc\\local\\share\\include"
+#define SDCC_INCLUDE_DIR      "\\sdcc\\include"
+#define SDCC_LIB_DIR          "\\sdcc\\lib"
+
+#define STD_LIB               "libsdcc"
+#define STD_INT_LIB           "libint"
+#define STD_LONG_LIB          "liblong"
+#define STD_FP_LIB            "libfloat"
+#define STD_DS390_LIB         "libds390"
+
+// #undef HAVE_SYS_SOCKET_H
+// #undef HAVE_SYS_ISA_DEFS_H	
+// #undef HAVE_ENDIAN_H
+
+#undef HAVE_STRERROR
+
+#undef OPT_DISABLE_MCS51
+#undef OPT_DISABLE_GBZ80
+#undef OPT_DISABLE_Z80
+#undef OPT_DISABLE_AVR
+#undef OPT_DISABLE_TININative
+#undef OPT_DISABLE_PIC
+#undef OPT_DISABLE_XA51
+
+#endif  /* SDCC_VC_HEADER */
+
+/* End of sdcc_vc.h */
