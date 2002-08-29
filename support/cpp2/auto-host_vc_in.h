1f8b2ca3c2bd5645e0a8891bf74707582a76ca50
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2002-08-29 18:33:59 +0000

    re-definition of O_BINARY and other O_XXX as _O_BINARY and _O_XXX
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@2091 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/cpp2/auto-host_vc_in.h
+++ support/cpp2/auto-host_vc_in.h
@@ -52,6 +52,17 @@ Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.*/
 #define close _close
 #define read _read
 #define write _write
+
+#define O_APPEND _O_APPEND
+#define O_CREAT  _O_CREAT
+#define O_EXCL   _O_EXCL
+#define O_RDONLY _O_RDONLY
+#define O_RDWR   _O_RDWR
+#define O_TRUNC  _O_TRUNC
+#define O_WRONLY _O_WRONLY
+#define O_BINARY _O_BINARY
+#define O_TEXT   _O_TEXT
+
 /*This one borrowed from \borland\bcc55\include\sys\stat.h*/
 #define S_IFBLK  0x3000  /* block special */
 
