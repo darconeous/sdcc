e3c8365473bdabc09514406cc4018c44a8887f49
bernhardheld <bernhardheld@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2003-03-21 22:53:02 +0000

    * support/scripts/sdcc_mingw32: adapted to configure from autoconf 2.54 * src/doc/cdbfileformat.lyx: added, written by Lenny Story * src/doc/Makefile: added cdbfileformat.lyx * src/doc/clean.mk: added cdbfileformat.lyx
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@2404 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- doc/clean.mk
+++ doc/clean.mk
@@ -1,8 +1,9 @@
 clean:
 # The directories are still in CVS, so don't delete them
 #       rm -rf $(MANUAL).html $(TSS).html
-	rm -f $(MANUAL).html/* $(TSS).html/*
-	rm -rf $(MANUAL).txt $(TSS).txt *.pdf *.tex *.aux *.dvi *.idx *.ilg \
+	rm -rf $(MANUAL).html/* $(TSS).html/* $(CDB).html
+	rm -rf $(MANUAL).txt $(TSS).txt $(CDB).txt \
+		*.pdf *.tex *.aux *.dvi *.idx *.ilg \
 		*.ind *.log *.toc *~ \#* *.ps */*.css */*.pl *.gif core *.glo
 
 # Deleting all files created by configuring or building the program
