fd593a3df97b2d4742d2c81ba81d3edfdf5ac342
bernhardheld <bernhardheld@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2003-05-22 15:28:36 +0000

    make z80 clearly failing this test
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@2640 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/regression/tests/zeropad.c
+++ support/regression/tests/zeropad.c
@@ -5,7 +5,6 @@
 #include <testfwk.h>
 
 #if defined(PORT_HOST) || defined(SDCC_z80) || defined(SDCC_gbz80)
-# define data
 # define idata
 # define xdata
 # define code
@@ -16,7 +15,7 @@ struct x {
   char  b[10];
 };
 
-struct x {storage} teststruct[3] = {
+struct x {storage} teststruct[6] = {
   { 10, {  1, 2, 3, 4, 5} },
   { 20, { 11 } },
   { 30, {  6, 7, 8} }
@@ -25,11 +24,12 @@ struct x {storage} teststruct[3] = {
 void
 testZeropad(void)
 {
+  ASSERT(teststruct[0].b[1] ==  2);
   ASSERT(teststruct[0].b[5] ==  0);
   ASSERT(teststruct[1].b[0] == 11);
 
   ASSERT(sizeof(teststruct[2].a) ==  2);
   ASSERT(sizeof(teststruct[1].b) == 10);
   ASSERT(sizeof(teststruct[1])   == 12);
-  ASSERT(sizeof(teststruct)      == 36);
+  ASSERT(sizeof(teststruct)      == 72);
 }
