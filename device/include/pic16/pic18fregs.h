b4ec8a5b409e8ec14fffd9152ea97ec93feee87d
vrokas <vrokas@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2004-02-16 23:11:45 +0000

    * doc/sdccman.lyx: added more pic16 port documentation * device/include/pic16: added header pic18fregs.h
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@3203 4a8a32a2-be11-0410-ad9d-d568d2c75423

---
+++ device/include/pic16/pic18fregs.h
@@ -0,0 +1,45 @@
+
+/*
+ * pic18fregs.h - 18F Family Device Library Header
+ *
+ * This file is part of the GNU PIC Library.
+ *
+ * January, 2004
+ * The GNU PIC Library is maintained by,
+ * 	Vangelis Rokas <vrokas@otenet.gr>
+ *
+ * $Id$
+ *
+ */
+
+#ifndef __PIC18FREGS_H__
+#define __PIC18FREGS_H__
+
+
+#if defined(pic18f242)
+#  include <pic18f248.h>
+
+#elif defined(pic18f252)
+#  include <pic18f252.h>
+
+#elif defined(pic18f258)
+#  include <pic18f258.h>
+
+#elif defined(pic18f442)
+#  include <pic18f442.h>
+
+#elif defined(pic18f448)
+#  include <pic18f448.h>
+
+#elif defined(pic18f452)
+#  include <pic18f452.h>
+
+#elif defined(pic18f458)
+# include <pic18f458.h>
+
+#else
+#  error Unsupported processor
+
+#endif
+
+#endif /* __PIC18FREGS_H__ */
