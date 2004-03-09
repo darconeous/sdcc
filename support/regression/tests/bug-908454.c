cfe68c56bf38978e8d38fc7356bee7b84ec7b9a6
bernhardheld <bernhardheld@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2004-03-09 10:48:33 +0000

    bit isn't available on serveral ports
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@3258 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/regression/tests/bug-908454.c
+++ support/regression/tests/bug-908454.c
@@ -2,7 +2,11 @@
 
 #include <testfwk.h>
 
-#if defined(PORT_HOST)
+#if defined(PORT_HOST) || defined(SDCC_z80) || defined(SDCC_gbz80) || defined(SDCC_hc08)
+#  define NO_BIT_TYPE
+#endif
+
+#if defined(NO_BIT_TYPE)
 volatile int a = 1, b = 1;
 #else
 volatile bit a = 1, b = 1;
