1cfe2b625119c327528ad29dec667719829b27ab
maartenbrock <maartenbrock@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2004-06-02 18:32:25 +0000

    * support/regression/tests/zeropad.c: added TEST_G macro for alpha build
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@3340 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/regression/tests/zeropad.c
+++ support/regression/tests/zeropad.c
@@ -8,8 +8,11 @@
 
 #if defined __GNUC__
   #define FLEXARRAY (__GNUC__ >= 3)
+  //since g fails on GCC 2.95.4 on alpha and I don't know how to detect alpha...
+  #define TEST_G    (__GNUC__ >= 3)
 #else
   #define FLEXARRAY 1
+  #define TEST_G    1
 #endif
 
 #include <testfwk.h>
@@ -25,14 +28,16 @@ typedef unsigned int size_t;
 
 char STORAGE array[5] = {'a', 'b', 'c'};
 
-struct w {
-  char a;
-  int  b;
-} STORAGE g[3] = {
-  {'x', 1},
-  {'y'},
-  {'z', 3}
-};
+#if TEST_G
+  struct w {
+    char a;
+    int  b;
+  } STORAGE g[3] = {
+    {'x', 1},
+    {'y'},
+    {'z', 3}
+  };
+#endif
 
 struct x {
   short a;
@@ -62,10 +67,12 @@ testZeropad(void)
   ASSERT(array[2] == 'c');
   ASSERT(array[4] == 0);
 
+#if TEST_G
   ASSERT(g[1].a == 'y');
   ASSERT(g[1].b == 0);
   ASSERT(g[2].a == 'z');
   ASSERT(g[2].b == 3);
+#endif
 
   ASSERT(teststruct[0].b[1] ==  2);
   ASSERT(teststruct[0].b[5] ==  0);
