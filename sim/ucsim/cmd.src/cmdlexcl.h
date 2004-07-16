7fe573ea83f6e0762dc73bc493f15fa2a8f6273d
drdani <drdani@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2004-07-16 16:32:30 +0000

    new files of 0.5.2
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@3382 4a8a32a2-be11-0410-ad9d-d568d2c75423

---
+++ sim/ucsim/cmd.src/cmdlexcl.h
@@ -0,0 +1,41 @@
+#ifndef CMDLEXCL_HEADER
+#define CMDLEXCL_HEADER
+
+#ifndef __FLEX_LEXER_H
+#include <FlexLexer.h>
+#endif
+
+#include "pobjcl.h"
+
+
+class cl_ucsim_lexer: public yyFlexLexer
+{
+protected:
+  char *string_to_parse;
+  char *string_ptr;
+public:
+  cl_ucsim_lexer(void): yyFlexLexer()
+  {
+    string_to_parse= string_ptr= 0;
+  }
+  cl_ucsim_lexer(char *str): yyFlexLexer()
+  {
+    string_to_parse= string_ptr= strdup(str);
+  }
+  virtual ~cl_ucsim_lexer(void)
+  {
+    if (string_to_parse)
+      {
+	free(string_to_parse);
+	string_to_parse= 0;
+      }
+  }
+  void activate_lexer_to_parse_into(void *yylv);
+protected:
+  int LexerInput(char *buf, int max_size);
+public:
+  int check_id(char *token);
+};
+
+
+#endif
