952c03bdf5f25163a7daeeb7141a90c30bc26fce
vrokas <vrokas@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2004-10-01 14:31:24 +0000

    device/include/asm/pic16/features.h: NEW
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@3511 4a8a32a2-be11-0410-ad9d-d568d2c75423

---
+++ device/include/asm/pic16/features.h
@@ -0,0 +1,24 @@
+/*
+ * PIC16 port features.
+ *
+ * This file is part of the GNU PIC Library.
+ *
+ * January, 2004
+ * The GNU PIC Library is maintained by,
+ * 	Vangelis Rokas <vrokas@otenet.gr>
+ *
+ * $Id$
+ *
+ */
+#ifndef __PIC16_ASM_FEATURES_H
+#define __PIC16_ASM_FEATURES_H   1
+
+#if 0
+#define _IL_REENTRANT	reentrant
+#define _FS_REENTRANT	reentrant
+#else
+#define _IL_REENTRANT
+#define _FS_REENTRANT
+#endif
+
+#endif
