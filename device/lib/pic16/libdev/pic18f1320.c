6fb246dc350f69b23fe06fb4c0ea792dd25e904f
tecodev <tecodev@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2005-10-11 11:00:12 +0000

    * device/lib/pic16/libio/i2c.ignore: added 1320, fixes broken builds * device/lib/pic16/libdev/pic18f1320.c: added 1320's device library
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@3904 4a8a32a2-be11-0410-ad9d-d568d2c75423

---
+++ device/lib/pic16/libdev/pic18f1320.c
@@ -0,0 +1,18 @@
+/* 
+ * pic18f1320.c - PIC18F1320 Device Library Sources
+ * 
+ * This file is part of the GNU PIC Library.
+ * 
+ * May, 2005
+ * The GNU PIC Library is maintained by
+ *     Raphael Neider <rneider@web.de>
+ * 
+ * originally designed by
+ *     Vangelis Rokas <vrokas@otenet.gr>
+ * 
+ * $Id$
+ * 
+ */
+
+#include "pic18f1220.c" /* just line, but with 8KB code */
+
