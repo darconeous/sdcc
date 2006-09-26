dda4245dd181f12b29f533f18e9fada31abced24
MaartenBrock <MaartenBrock@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2006-09-26 19:40:27 +0000

    * src/z80/peeph.def: updated conditonals, fixed bug 1565908
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@4386 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- device/lib/rand.c
+++ device/lib/rand.c
@@ -29,7 +29,7 @@ static unsigned long int next = 1;
 int rand(void)
 {
     next = next * 1103515245UL + 12345;
-    return (unsigned int)(next/65536) % (RAND_MAX + 1);
+    return (unsigned int)(next/65536) % (RAND_MAX + 1U);
 }
 
 void srand(unsigned int seed)
