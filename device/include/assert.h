c58267e765da5b700374b35797ff11b42d874fe7
MaartenBrock <MaartenBrock@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2007-03-24 19:22:56 +0000

    * device/include/assert.h: applied patch 1686533 to fix bug 1686528,   thanks Alexey Shamrin <shamrin AT gmail.com>,   made assert.h header ANSI compliant (added a check for NDEBUG macro,   removed header guard) * .version, * sdcc.spec: bumped version to 2.6.5
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@4711 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- device/include/assert.h
+++ device/include/assert.h
@@ -7,26 +7,32 @@
    under the terms of the GNU General Public License as published by the
    Free Software Foundation; either version 2, or (at your option) any
    later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-   
+
    In other words, you are welcome to use, share and improve this program.
    You are forbidden to forbid anyone else to use, share and improve
-   what you give them.   Help stamp out software-hoarding!  
+   what you give them.   Help stamp out software-hoarding!
 -------------------------------------------------------------------------*/
 
-#ifndef SDCC_ASSERT_H
-#define SDCC_ASSERT_H
+#undef assert
 
+#ifdef NDEBUG
+
+/* Debugging disabled -- do not evaluate assertions. */
+#define assert(x) ((void)0)
+
+#else
+
+/* Debugging enabled -- verify assertions at run time. */
 void _assert(char *, const char *, unsigned int);
 #define assert(x) ((x) == 0 ? _assert(#x, __FILE__, __LINE__):(void)0)
-
 
 #endif
