62ef71465876383f7217fcfa4675e552e9d9fbe4
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2007-04-22 16:59:19 +0000

    "__code" keyword replaces with "code" macro
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@4765 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/regression/tests/bug-1699804.c
+++ support/regression/tests/bug-1699804.c
@@ -4,12 +4,12 @@
 
 #include <testfwk.h>
 
-static __code const char Str_global[] = "Hello1";
+static code const char Str_global[] = "Hello1";
 char s1, s2;
 
 void testFoo(void)
 {
-  static __code const char str_local[] = "Hello2";
+  static code const char str_local[] = "Hello2";
 
   static char str1[sizeof(Str_global)];
   static char str2[sizeof(str_local)]; // causes error 20: Undefined identifier 'str_local'
