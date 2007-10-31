fadd2e4872075e85bf61f52c52c9c7b7ec06e333
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2007-10-31 18:00:54 +0000

    fixed regtest #1805702
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@4957 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/regression/tests/bug-1805702.c
+++ support/regression/tests/bug-1805702.c
@@ -18,15 +18,7 @@ test(void)
 
 float __fsmul (float, float);
 
-/* multiply two floats */
 float __fsmul (float a1, float a2) {
+  /* just for tesing... */
   return (a1 + a2);
-}
-
-extern unsigned char _divuchar (unsigned char a, unsigned char b);
-
-signed char
-_divschar (signed char a, signed char b)
-{
-  return _divuchar ((unsigned int)a, (unsigned int)b);
 }
