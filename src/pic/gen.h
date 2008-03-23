6cb515b98c5977589446a39a6271958383d0a3da
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2008-03-23 07:57:27 +0000

    * src/pic/gen.h: Q&D solution for the bug, found by Jim Paris
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@5115 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- src/pic/gen.h
+++ src/pic/gen.h
@@ -2,25 +2,25 @@
   SDCCgen51.h - header file for code generation for 8051
 
              Written By -  Sandeep Dutta . sandeep.dutta@usa.net (1998)
-	     PIC port   - T. Scott Dattalo scott@dattalo.com (2000)
+             PIC port   - T. Scott Dattalo scott@dattalo.com (2000)
 
    This program is free software; you can redistribute it and/or modify it
    under the terms of the GNU General Public License as published by the
    Free Software Foundation; either version 2, or (at your option) any
    later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-   
+
    In other words, you are welcome to use, share and improve this program.
    You are forbidden to forbid anyone else to use, share and improve
-   what you give them.   Help stamp out software-hoarding!  
+   what you give them.   Help stamp out software-hoarding!
 -------------------------------------------------------------------------*/
 
 #ifndef SDCCGENPIC14_H
@@ -28,17 +28,17 @@
 
 extern int debug_verbose;
 
-#define FENTRY do { 									\
-	/*fprintf (stderr, "%s:%u:%s: *{*\n", __FILE__, __LINE__, __FUNCTION__);*/	\
-	if (options.debug || debug_verbose) {						\
-		emitpComment ("; %s:%u:%s *{*", __FILE__, __LINE__, __FUNCTION__);	\
-	}										\
+#define FENTRY do {                                                                     \
+        /*fprintf (stderr, "%s:%u:%s: *{*\n", __FILE__, __LINE__, __FUNCTION__);*/      \
+        if (options.debug || debug_verbose) {                                           \
+                emitpComment ("; %s:%u:%s *{*", __FILE__, __LINE__, __FUNCTION__);      \
+        }                                                                               \
 } while (0)
-#define FEXIT do { 									\
-	/*fprintf (stderr, "%s:%u:%s: *}*\n", __FILE__, __LINE__, __FUNCTION__);*/	\
-	if (options.debug || debug.verbose) {						\
-		emitpComment ("; %s:%u:%s *}*", __FILE__, __LINE__, __FUNCTION__);	\
-	}										\
+#define FEXIT do {                                                                      \
+        /*fprintf (stderr, "%s:%u:%s: *}*\n", __FILE__, __LINE__, __FUNCTION__);*/      \
+        if (options.debug || debug.verbose) {                                           \
+                emitpComment ("; %s:%u:%s *}*", __FILE__, __LINE__, __FUNCTION__);      \
+        }                                                                               \
 } while (0)
 
 struct pCodeOp;
@@ -46,34 +46,38 @@ struct pCodeOp;
 enum
 {
   AOP_LIT = 1,
-  AOP_REG, AOP_DIR,
-  AOP_STK, AOP_IMMD, AOP_STR,
+  AOP_REG,
+  AOP_DIR,
+  AOP_STK,
+  AOP_IMMD,
+  AOP_STR,
   AOP_CRY,
-  AOP_PCODE
+  AOP_ACC,
+  AOP_PCODE = 13
 
 };
 
-/* type asmop : a homogenised type for 
+/* type asmop : a homogenised type for
    all the different spaces an operand can be
    in */
 typedef struct asmop
 {
 
   short type;  /* can have values
-		  AOP_LIT    -  operand is a literal value
-		  AOP_REG    -  is in registers
-		  AOP_DIR    -  direct just a name
-		  AOP_STK    -  should be pushed on stack this
-		  can happen only for the result
-		  AOP_IMMD   -  immediate value for eg. remateriazable 
-		  AOP_CRY    -  carry contains the value of this
-		  AOP_STR    -  array of strings
-	       */
-  short coff;	        /* current offset */
-  short size;	        /* total size */
+                  AOP_LIT    -  operand is a literal value
+                  AOP_REG    -  is in registers
+                  AOP_DIR    -  direct just a name
+                  AOP_STK    -  should be pushed on stack this
+                  can happen only for the result
+                  AOP_IMMD   -  immediate value for eg. remateriazable
+                  AOP_CRY    -  carry contains the value of this
+                  AOP_STR    -  array of strings
+               */
+  short coff;           /* current offset */
+  short size;           /* total size */
   unsigned code:1;      /* is in Code space */
-  unsigned paged:1;	/* in paged memory  */
-  unsigned freed:1;	/* already freed    */
+  unsigned paged:1;     /* in paged memory  */
+  unsigned freed:1;     /* already freed    */
   union
   {
     value *aop_lit;     /* if literal */
@@ -140,7 +144,7 @@ extern unsigned fReturnSizePic;
 
 int pic14_getDataSize(operand *op);
 void emitpcode_real(PIC_OPCODE poc, pCodeOp *pcop);
-#define emitpcode(poc,pcop)	do { if (options.debug || debug_verbose) { emitpComment (" >>> %s:%d:%s", __FILE__, __LINE__, __FUNCTION__); } emitpcode_real(poc,pcop); } while(0)
+#define emitpcode(poc,pcop)     do { if (options.debug || debug_verbose) { emitpComment (" >>> %s:%d:%s", __FILE__, __LINE__, __FUNCTION__); } emitpcode_real(poc,pcop); } while(0)
 void emitpComment (const char *fmt, ...);
 void emitpLabel(int key);
 void pic14_emitcode (char *inst,char *fmt, ...);
