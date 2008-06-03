ca2371c2217c453b1325ac8e2dfb79f6056bb505
MaartenBrock <MaartenBrock@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2008-06-03 17:42:20 +0000

    * device/include/asm/ds390/features.h, * device/include/asm/mcs51/features.h, * device/include/mcs51/regc515c.h: inserted LGPL notice
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@5190 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- device/include/asm/mcs51/features.h
+++ device/include/asm/mcs51/features.h
@@ -1,5 +1,23 @@
-/** MCS51 specific features.
- */
+/*---------------------------------------------------------------------------
+   MCS51 specific features.
+
+   Copyright (C) 2004 - Maarten Brock, sourceforge.brock@dse.nl
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+---------------------------------------------------------------------------*/
+
 #ifndef __SDC51_ASM_MCS51_FEATURES_H
 #define __SDC51_ASM_MCS51_FEATURES_H   1
 
