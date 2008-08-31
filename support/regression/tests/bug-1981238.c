92ccc669ca06792950f4be64c9e510df30c60b59
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2008-08-31 10:47:20 +0000

    fix to pass the host regression test
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@5226 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/regression/tests/bug-1981238.c
+++ support/regression/tests/bug-1981238.c
@@ -5,23 +5,23 @@
 
 code struct {
   char x:1;
-  char :0;
+  char  :0;
   char d:2;
   char b:6;
-} pad ={1, 2, 1};
+} pad = {1, 2, 1};
 
 code struct {
   int p:1;
   int q:7;
-  int :0;
+  char :0;
   int s:1;
   int t:7;
-} noPad ={1, 120, 1, 1, 127};
+} noPad = {1, 120, 1, 1, 127};
 
 code struct {
-  char :0;
+  char  :0;
   char b;
-} initialNoPad[] ={{1, 2}, {3,4}};
+} initialNoPad[] = {{1, 2}, {3,4}};
 
 void testBitfield (void)
 {
