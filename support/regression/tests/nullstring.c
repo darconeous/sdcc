385efa9f2ada438f1ba7d7dd92f299758f8ca514
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2008-09-07 13:05:28 +0000

    * support/regression/tests/nullstring.c: 	  disable warning about excess elements in array of chars initializer
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@5232 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/regression/tests/nullstring.c
+++ support/regression/tests/nullstring.c
@@ -4,24 +4,28 @@
 */
 #include <testfwk.h>
 
+#ifndef PORT_HOST
+#pragma disable_warning 147 //no warning about excess elements in array of chars initializer
+#endif
+
 {storage} char string1[] = "";
 {storage} char string2[] = "a\0b\0c";
 {storage} char string3[5] = "a\0b\0c";
 
 void
-testStringArray(void)
+testStringArray (void)
 {
   /* Make sure the strings are the correct size */
   /* and have the terminating null character */
-  ASSERT(sizeof(string1)==1);
-  ASSERT(sizeof(string2)==6);
-  ASSERT(sizeof(string3)==5);
-  ASSERT(string1[0]==0);
-  ASSERT(string2[5]==0);
+  ASSERT (sizeof (string1) == 1);
+  ASSERT (sizeof (string2) == 6);
+  ASSERT (sizeof (string3) == 5);
+  ASSERT (string1[0] == 0);
+  ASSERT (string2[5] == 0);
 
-  ASSERT(string2[0]=='a');
-  ASSERT(string2[2]=='b');
-  ASSERT(string2[4]=='c');
+  ASSERT (string2[0]== 'a');
+  ASSERT (string2[2]== 'b');
+  ASSERT (string2[4]== 'c');
 
 }
 
@@ -31,6 +35,6 @@ testStringConst(void)
   char * constStr1 = "";
   char * constStr2 = "a\0b\0c";
 
-  ASSERT (constStr1[0]==0);
-  ASSERT (constStr2[5]==0);
+  ASSERT (constStr1[0] == 0);
+  ASSERT (constStr2[5] == 0);
 }
