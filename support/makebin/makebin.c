daa2d0fe0b48388c0e5b0864725d7c888d4a2b32
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2010-02-20 17:48:26 +0000

    * support/makebin/makebin.c:   added optional input and output file names arguments,   making it (hopefully) more robust, ...
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@5700 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/makebin/makebin.c
+++ support/makebin/makebin.c
@@ -2,6 +2,7 @@
   makebin - turn a .ihx file into a binary image or GameBoy format binaryimage
 
   Copyright (c) 2000 Michael Hope
+  Copyright (c) 2010 Borut Razem
 
   This software is provided 'as-is', without any express or implied
   warranty.  In no event will the authors be held liable for any damages
@@ -18,8 +19,6 @@
   2. Altered source versions must be plainly marked as such, and must not be
      misrepresented as being the original software.
   3. This notice may not be removed or altered from any source distribution.
-
-  Michael Hope
 */
 
 
@@ -38,23 +37,45 @@
 
 typedef unsigned char BYTE;
 
-#define FILL_BYTE 0xFF
+#define FILL_BYTE 0xff
 
 int
-getnibble (char **p)
+getnibble (FILE *fin)
 {
-  int ret = *((*p)++) - '0';
+  int ret;
+  int c = getc (fin);
+
+  if (feof (fin) || ferror (fin))
+    {
+      fprintf (stderr, "error: unexpected end of file.\n");
+      exit (6);
+    }
+
+  ret = c - '0';
   if (ret > 9)
     {
       ret -= 'A' - '9' - 1;
+    }
+
+  if (ret > 0xf)
+    {
+       ret -= 'a' - 'A';
+    }
+
+  if (ret < 0 || ret > 0xf)
+    {
+      fprintf (stderr, "error: character %02x.\n", ret);
+      exit (7);
     }
   return ret;
 }
 
 int
-getbyte (char **p)
+getbyte (FILE *fin, int *sum)
 {
-  return (getnibble (p) << 4) | getnibble (p);
+  int b = (getnibble (fin) << 4) | getnibble (fin);
+  *sum += b;
+  return b;
 }
 
 void
@@ -62,26 +83,29 @@ usage (void)
 {
   fprintf (stderr,
            "makebin: convert a Intel IHX file to binary or GameBoy format binary.\n"
-           "Usage: makebin [-p] [-s romsize] [-h] [-Z] [-yo rem_banks] [-ya rem_banks] [-yt mbc_type] [-yn name]\n"
+           "Usage: makebin [options] [<in_file> [<out_file>]]\n"
            "Options:\n"
            "  -p             pack mode: the binary file size will be truncated to the last occupied byte\n"
            "  -s romsize     size of the binary file (default: 32768)\n"
            "  -Z             genarate GameBoy format binary file\n"
-           "GameBoy format options:\n"
+           "GameBoy format options (applicable only with -Z option):\n"
            "  -yo            number of rom banks (default: 2)\n"
            "  -ya            number of ram banks (default: 0)\n"
            "  -yt            MBC type (default: no MBC)\n"
-           "  -yn            cartrige name (default: none)\n");
+           "  -yn            cartrige name (default: none)\n"
+           "Arguments>\n"
+           "  <in_file>      optional IHX input file, '-' means stdin. (default: stdin)\n"
+           "  <out_file>     optional output file, '-' means stdout. (default: stdout)\n");
 }
 
 #define CART_NAME_LEN 16
 
 struct gb_opt_s
 {
-  char cart_name[CART_NAME_LEN];        /* cartrige name buffer */
-  BYTE mbc_type;                /* MBC type (default: no MBC) */
-  short nb_rom_banks;           /* Number of rom banks (default: 2) */
-  BYTE nb_ram_banks;            /* Number of ram banks (default: 0) */
+  char cart_name[CART_NAME_LEN];  /* cartrige name buffer */
+  BYTE mbc_type;                  /* MBC type (default: no MBC) */
+  short nb_rom_banks;             /* Number of rom banks (default: 2) */
+  BYTE nb_ram_banks;              /* Number of ram banks (default: 0) */
 };
 
 void
@@ -172,7 +196,7 @@ gb_postproc (BYTE * rom, int size, int *real_size, struct gb_opt_s *o)
       break;
 
     default:
-      fprintf (stderr, "WARNING: unsupported number of ROM banks (%d)\n", o->nb_rom_banks);
+      fprintf (stderr, "warning: unsupported number of ROM banks (%d)\n", o->nb_rom_banks);
       rom[0x148] = 0;
       break;
     }
@@ -204,28 +228,84 @@ gb_postproc (BYTE * rom, int size, int *real_size, struct gb_opt_s *o)
       break;
 
     default:
-      fprintf (stderr, "WARNING: unsupported number of RAM banks (%d)\n", o->nb_ram_banks);
+      fprintf (stderr, "warning: unsupported number of RAM banks (%d)\n", o->nb_ram_banks);
       rom[0x149] = 0;
       break;
     }
 
   /* Update complement checksum */
   chk = 0;
-  for (i = 0x134; i < 0x14D; ++i)
+  for (i = 0x134; i < 0x14d; ++i)
     chk += rom[i];
-  rom[0x014D] = (unsigned char) (0xE7 - (chk & 0xFF));
+  rom[0x014d] = (unsigned char) (0xe7 - (chk & 0xff));
 
   /* Update checksum */
   chk = 0;
-  rom[0x14E] = 0;
-  rom[0x14F] = 0;
+  rom[0x14e] = 0;
+  rom[0x14f] = 0;
   for (i = 0; i < size; ++i)
     chk += rom[i];
-  rom[0x14E] = (unsigned char) ((chk >> 8) & 0xFF);
-  rom[0x14F] = (unsigned char) (chk & 0xFF);
+  rom[0x14e] = (unsigned char) ((chk >> 8) & 0xff);
+  rom[0x14f] = (unsigned char) (chk & 0xff);
 
   if (*real_size < 0x150)
     *real_size = 0x150;
+}
+
+int
+read_ihx (FILE *fin, BYTE *rom, int size, int *real_size)
+{
+  int record_type;
+
+  do
+    {
+      int nbytes;
+      int addr;
+      int checksum, sum = 0;
+
+      if (getc (fin) != ':')
+        {
+          fprintf (stderr, "error: invalid IHX line.\n");
+          return 0;
+        }
+      nbytes = getbyte (fin, &sum);
+      addr = getbyte (fin, &sum) << 8 | getbyte (fin, &sum);
+      record_type = getbyte (fin, &sum);
+      if (record_type > 1)
+        {
+          fprintf (stderr, "error: unsupported record type: %02x.\n", record_type);
+          return 0;
+        }
+
+      if (addr + nbytes >= size)
+        {
+          fprintf (stderr, "error: size of the buffer is too small.\n");
+          return 0;
+        }
+
+      while (nbytes--)
+        {
+          if (addr < size)
+            rom[addr++] = getbyte (fin, &sum);
+        }
+
+      if (addr > *real_size)
+        *real_size = addr;
+
+      checksum = getbyte (fin, &sum);
+      if (0 != (sum & 0xff))
+        {
+          fprintf (stderr, "error: bad checksum: %02x.\n", checksum);
+          return 0;
+        }
+
+      while (isspace (sum = getc (fin)))  /* skip all kind of speces */
+        ;
+      ungetc (sum, fin);
+    }
+  while (1 != record_type); /* EOF record */
+
+  return 1;
 }
 
 int
@@ -233,35 +313,25 @@ main (int argc, char **argv)
 {
   int size = 32768, pack = 0, real_size = 0;
   BYTE *rom;
-  char line[256];
-  char *p;
+  FILE *fin, *fout;
+  int ret;
   int gb = 0;
   struct gb_opt_s gb_opt = { "", 0, 2, 0 };
-
-  argc--;
-  argv++;
 
 #if defined(_WIN32)
   setmode (fileno (stdout), O_BINARY);
 #endif
 
-  while (argc--)
+  while (*++argv && '-' == argv[0][0] && '\0' != argv[0][1])
     {
-      if (**argv != '-')
-        {
-          usage ();
-          return 1;
-        }
       switch (argv[0][1])
         {
         case 's':
-          if (argc < 1)
+          if (!++argv)
             {
               usage ();
               return 1;
             }
-          argc--;
-          argv++;
           size = atoi (*argv);
           break;
 
@@ -288,46 +358,38 @@ main (int argc, char **argv)
           switch (argv[0][2])
             {
             case 'o':
-              if (argc < 1)
+              if (!++argv)
                 {
                   usage ();
                   return 1;
                 }
-              argc--;
-              argv++;
               gb_opt.nb_rom_banks = atoi (*argv);
               break;
 
             case 'a':
-              if (argc < 1)
+              if (!++argv)
                 {
                   usage ();
                   return 1;
                 }
-              argc--;
-              argv++;
               gb_opt.nb_ram_banks = atoi (*argv);
               break;
 
             case 't':
-              if (argc < 1)
+              if (!++argv)
                 {
                   usage ();
                   return 1;
                 }
-              argc--;
-              argv++;
               gb_opt.mbc_type = atoi (*argv);
               break;
 
             case 'n':
-              if (argc < 1)
+              if (!++argv)
                 {
                   usage ();
                   return 1;
                 }
-              argc--;
-              argv++;
               strncpy (gb_opt.cart_name, *argv, CART_NAME_LEN);
               break;
 
@@ -341,7 +403,22 @@ main (int argc, char **argv)
           usage ();
           return 1;
         }
-      argv++;
+    }
+
+  fin = stdin;
+  fout = stdout;
+  if (*argv)
+    {
+      if ('-' != argv[0][0] || '\0' != argv[0][1])
+        fin = fopen (*argv, "r");
+
+      ++argv;
+    }
+
+  if (NULL != argv[0] && NULL != argv[1])
+    {
+      usage ();
+      return 1;
     }
 
   if (gb && size != 32768)
@@ -357,41 +434,26 @@ main (int argc, char **argv)
       return 1;
     }
   memset (rom, FILL_BYTE, size);
-  while (fgets (line, 256, stdin) != NULL)
+
+  ret = read_ihx (fin, rom, size, &real_size);
+
+  fclose (fin);
+
+  if (ret)
     {
-      int nbytes;
-      int addr;
+      if (gb)
+        gb_postproc (rom, size, &real_size, &gb_opt);
 
-      if (*line != ':')
+      if (*argv)
         {
-          fprintf (stderr, "error: invalid IHX line.\n");
-          return 2;
-        }
-      p = line + 1;
-      nbytes = getbyte (&p);
-      addr = getbyte (&p) << 8 | getbyte (&p);
-      getbyte (&p);
-
-      if (addr + nbytes >= size)
-        {
-          fprintf (stderr, "error: size of the buffer is too small.\n");
-          return 3;
+          if ('-' != argv[0][0] || '\0' != argv[0][1])
+            fout = fopen (*argv, "wb");
         }
 
-      while (nbytes--)
-        {
-          if (addr < size)
-            rom[addr++] = getbyte (&p);
-        }
+      fwrite (rom, 1, (pack ? real_size : size), fout);
 
-      if (addr > real_size)
-        real_size = addr;
+      fclose (fout);
     }
-
-  if (gb)
-    gb_postproc (rom, size, &real_size, &gb_opt);
-
-  fwrite (rom, 1, (pack ? real_size : size), stdout);
 
   return 0;
 }
