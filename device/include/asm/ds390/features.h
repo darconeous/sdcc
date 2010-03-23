4c8f434820f5f90863eae555e998216456f25266
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2010-03-23 22:27:48 +0000

    * device/include/asm/ds390/features.h,   device/lib/ds390/memcpyx.c, device/lib/ds390/putchar.c:   sdcc library license changed to GPL+LE
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@5750 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- device/include/asm/ds390/features.h
+++ device/include/asm/ds390/features.h
@@ -1,22 +1,30 @@
-/*---------------------------------------------------------------------------
-   DS390/DS400 specific features.
+/*-------------------------------------------------------------------------
+   features.h - DS390/DS400 specific features.
 
-   Copyright (C) 2004 - Maarten Brock, sourceforge.brock@dse.nl
+   Copyright (C) 2004, Maarten Brock, sourceforge.brock@dse.nl
 
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
+   This library is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2.1, or (at your option) any
+   later version.
 
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
 
-   You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
----------------------------------------------------------------------------*/
+   You should have received a copy of the GNU General Public License 
+   along with this library; see the file COPYING. If not, write to the
+   Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
+   MA 02110-1301, USA.
+
+   As a special exception, if you link this library with other files,
+   some of which are compiled with SDCC, to produce an executable,
+   this library does not by itself cause the resulting executable to
+   be covered by the GNU General Public License. This exception does
+   not however invalidate any other reasons why the executable file
+   might be covered by the GNU General Public License.
+-------------------------------------------------------------------------*/
 
 #ifndef __SDC51_ASM_DS390_FEATURES_H
 #define __SDC51_ASM_DS390_FEATURES_H   1
