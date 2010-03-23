fc1eda4b549cdad7b20c354e3f197b68ecb25d6a
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2010-03-23 22:15:48 +0000

    * device/include/*.h, device/include/asm/z80/features.h,   device/include/asm/default/features.h, device/include/z80/*.h,   device/lib/z80/*.s: sdcc library license changed to GPL+LE
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@5749 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- device/include/asm/default/features.h
+++ device/include/asm/default/features.h
@@ -3,14 +3,14 @@
 
    Copyright (C) 2001, Michael Hope
 
-   This program is free software; you can redistribute it and/or modify it
+   This library is free software; you can redistribute it and/or modify it
    under the terms of the GNU General Public License as published by the
-   Free Software Foundation; either version 2, or (at your option) any
+   Free Software Foundation; either version 2.1, or (at your option) any
    later version.
 
-   This program is distributed in the hope that it will be useful,
+   This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License 
@@ -21,7 +21,7 @@
    As a special exception, if you link this library with other files,
    some of which are compiled with SDCC, to produce an executable,
    this library does not by itself cause the resulting executable to
-   be covered by the GNU General Public License.  This exception does
+   be covered by the GNU General Public License. This exception does
    not however invalidate any other reasons why the executable file
    might be covered by the GNU General Public License.
 -------------------------------------------------------------------------*/
