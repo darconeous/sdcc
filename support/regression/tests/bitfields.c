ffabdd0fda5979fa6e36ea5c48d79ebc58de2c83
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2010-03-26 05:49:55 +0000

    - bitfields allocation is platform dependent, not compiler dependent
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@5758 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/regression/tests/bitfields.c
+++ support/regression/tests/bitfields.c
@@ -343,8 +343,8 @@ testBitfields(void)
 {
   c_bf.c0_3 = 2;
   c_bf.c3_5 = 3;
-#if defined(PORT_HOST) && (defined(__ppc__) || defined(__PPC__) || defined(__SUNPRO_C) || defined(__sparc64__))
-  /* bitfields on powerpc architecture are allocated from left to right */
+#if defined(PORT_HOST) && (defined(__ppc__) || defined(__PPC__) || defined(__sparc) || defined(__sparc64__))
+  /* bitfields on powerpc and sparc architectures are allocated from left to right */
   ASSERT(*(char *)(&c_bf) == ((2<<(8-3)) + 3) );
 #else
   ASSERT(*(char *)(&c_bf) == (2 + (3<<3)) );
