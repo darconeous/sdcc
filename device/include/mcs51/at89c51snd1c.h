e53e5576e3d8046b16ddc0494c6e855335125754
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2010-04-05 11:29:46 +0000

    * sdcc/sim/ucsim/gui.src/Makefile.in:   fixed for Solaris if CURSES_PKGS is empty * sdcc/device/include/mcs51/at89c51snd1c.h:   sdcc library license changed to GPL+LE
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@5782 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- device/include/mcs51/at89c51snd1c.h
+++ device/include/mcs51/at89c51snd1c.h
@@ -1,31 +1,34 @@
 /*-------------------------------------------------------------------------
-   Register Declarations for the Atmel AT89C51SND1C Processor
+   at89c51snd1c.h - Register Declarations for the Atmel AT89C51SND1C Processor
 
-   Written By -  Weston Schmidt <weston_schmidt@alumni.purdue.edu> (Sept 2005)
+   Copyright (C) 2005, Weston Schmidt <weston_schmidt@alumni.purdue.edu>
 
    This document is based on the AT8xC51SND1C document
        4109H-8051-01/05
 
-   $Id$
-
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
+   This library is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2.1, or (at your option) any
+   later version.
 
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
 
-   You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+   You should have received a copy of the GNU General Public License 
+   along with this library; see the file COPYING. If not, write to the
+   Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
+   MA 02110-1301, USA.
 
-   In other words, you are welcome to use, share and improve this program.
-   You are forbidden to forbid anyone else to use, share and improve
-   what you give them.   Help stamp out software-hoarding!
+   As a special exception, if you link this library with other files,
+   some of which are compiled with SDCC, to produce an executable,
+   this library does not by itself cause the resulting executable to
+   be covered by the GNU General Public License. This exception does
+   not however invalidate any other reasons why the executable file
+   might be covered by the GNU General Public License.
 -------------------------------------------------------------------------*/
+
 
 #ifndef __AT89C51SND1_H__
 #define __AT89C51SND1_H__
