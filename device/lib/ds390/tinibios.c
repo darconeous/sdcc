33548e55fe63928ed461fb959190e13f8ffa77e2
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2010-08-01 14:27:01 +0000

    * device/include/ds80c390.h, device/include/tinibios.h,   device/include/time.h, device/include/ds390/serial390.h,   device/lib/time.c, device/lib/gets.c, device/lib/ds390/lcd390.c,   device/lib/ds390/tinibios.c, device/lib/ds390/rtc390.c,   device/lib/ds390/i2c390.c, device/lib/ds400/tinibios.c:   sdcc library license changed to GPL+LE
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@5901 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- device/lib/ds390/tinibios.c
+++ device/lib/ds390/tinibios.c
@@ -1,25 +1,29 @@
 /*-------------------------------------------------------------------------
-  tinibios.c - startup and serial routines for the DS80C390 (tested on TINI)
-  
-   Written By - Johan Knol, johan.knol@iduna.nl
-    
-   This program is free software; you can redistribute it and/or modify it
+   tinibios.c - startup and serial routines for the DS80C390 (tested on TINI)
+
+   Copyright (C) 2001, Johan Knol <johan.knol AT iduna.nl>
+
+   This library is free software; you can redistribute it and/or modify it
    under the terms of the GNU General Public License as published by the
-   Free Software Foundation; either version 2, or (at your option) any
+   Free Software Foundation; either version 2.1, or (at your option) any
    later version.
-   
-   This program is distributed in the hope that it will be useful,
+
+   This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
    GNU General Public License for more details.
-   
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-   
-   In other words, you are welcome to use, share and improve this program.
-   You are forbidden to forbid anyone else to use, share and improve
-   what you give them.   Help stamp out software-hoarding!  
+
+   You should have received a copy of the GNU General Public License 
+   along with this library; see the file COPYING. If not, write to the
+   Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
+   MA 02110-1301, USA.
+
+   As a special exception, if you link this library with other files,
+   some of which are compiled with SDCC, to produce an executable,
+   this library does not by itself cause the resulting executable to
+   be covered by the GNU General Public License. This exception does
+   not however invalidate any other reasons why the executable file
+   might be covered by the GNU General Public License.
 -------------------------------------------------------------------------*/
 
 #include <tinibios.h>
