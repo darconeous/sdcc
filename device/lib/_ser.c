fa66ff5cc677d1a7a7787f416cc5aacacd42c221
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2010-08-05 19:23:55 +0000

    * device/include/mcs51/ser.h, device/lib/_ser.c:   sdcc library license changed to GPL+LE
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@5916 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- device/lib/_ser.c
+++ device/lib/_ser.c
@@ -1,3 +1,32 @@
+/*-------------------------------------------------------------------------
+   _ser.c - this file contains a simple interrupt driven serial driver with
+            buffer (no check for overflow!!!).
+
+   Copyright (C) 1999, Sandeep Dutta <sandeep.dutta AT ieee.org>
+
+   This library is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2.1, or (at your option) any
+   later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License 
+   along with this library; see the file COPYING. If not, write to the
+   Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
+   MA 02110-1301, USA.
+
+   As a special exception, if you link this library with other files,
+   some of which are compiled with SDCC, to produce an executable,
+   this library does not by itself cause the resulting executable to
+   be covered by the GNU General Public License. This exception does
+   not however invalidate any other reasons why the executable file
+   might be covered by the GNU General Public License.
+-------------------------------------------------------------------------*/
+
 /*KA******************************************************************
 * PROJECT: PL-One/8052 
 **********************************************************************
@@ -26,8 +55,6 @@
 * COMPILE TIME OPTIONS: -
 * DEBUG OPTIONS: -
 ******************************************************************KE*/
-/*      $Id$    */
-
 
 #include <8052.h>
 
