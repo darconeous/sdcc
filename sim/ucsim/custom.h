cebfae610f43320c55addd8ac5129e97ae485ee4
tecodev <tecodev@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2010-09-06 10:33:23 +0000

    * configure.in, debugger/mcs51/configure.in,   device/lib/pic/configure.in, device/lib/pic16/configure.ac,   sim/ucsim/configure.in, sim/ucsim/custom.h,   sim/ucsim/ddconfig_in.h, support/packihx/configure.in:   updated build system to allow autoreconf -fi to set up the project
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@5954 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sim/ucsim/custom.h
+++ sim/ucsim/custom.h
@@ -1,5 +1,5 @@
 /*
- * Simulator of microcontrollers (ddconfig.h)
+ * Simulator of microcontrollers (custom.h)
  *
  * Copyright (C) 1999,99 Drotos Daniel, Talker Bt.
  *
@@ -25,85 +25,12 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 02111-1307, USA. */
 /*@1@*/
 
-#ifndef DDCONFIG_HEADER
-#define DDCONFIG_HEADER
+#ifndef CUSTOM_HEADER
+#define CUSTOM_HEADER
 
-#define DD_TRUE  1
-#define DD_FALSE 0
-#define NIL     0
-
-#undef STDC_HEADERS
-#undef HAVE_GETOPT_H
-#undef HAVE_UNISTD_H
-#undef HAVE_DIRENT_H
-#undef HAVE_SYS_NDIR_H
-#undef HAVE_SYS_DIR_H
-#undef HAVE_NDIR_H
-#undef HAVE_SYS_SOCKET_H
-#undef HAVE_WINSOCK2_H
-#undef HAVE_DLFCN_H
-#undef HAVE_CURSES_H
-#undef HAVE_TERMIOS_H
-#undef HAVE_ENDIAN_H
-#undef HAVE_SYS_ENDIAN_H
-#undef HAVE_MACHINE_ENDIAN_H
-#undef HAVE_SYS_ISA_DEFS_H
-
-#undef SOCKET_AVAIL
-#undef SOCKLEN_T
-#undef UCSOCKET_T
-#undef HEADER_SOCKET
-
-#undef FD_NEED_TIME_H
-#undef FD_NEED_TYPES_H
-#undef FD_NEED_SELECT_H
-#undef FD_NEED_WINSOCK2_H
-#undef HEADER_FD
-#undef FD_HEADER_OK
-
-#undef SCANF_A
-#undef GNU_GETCWD
-
-#undef HAVE_STRLEN
-#undef HAVE_STRCPY
-#undef HAVE_STRCAT
-#undef HAVE_STRSTR
-#undef HAVE_STRCMP
-#undef HAVE_STRERROR
-#undef HAVE_STRTOK
-#undef HAVE_STRDUP
-#undef HAVE_STRCHR
-
-#undef HAVE_MEMCPY
-
-#undef HAVE_VPRINTF
-#undef HAVE_DOPRNT
-#undef HAVE_VSNPRINTF
-#undef HAVE__VSNPRINTF
-#undef HAVE_VASPRINTF
-
-#undef HAVE_GETLINE
-#undef HAVE_GETDELIM
-#undef HAVE_FGETS
-
-#undef HAVE_YYLEX
-#undef HAVE_FLEX
-#undef YYTEXT_POINTER
-
-#undef RETSIGTYPE
-#undef SIZEOF_CHAR
-#undef SIZEOF_SHORT
-#undef SIZEOF_INT
-#undef SIZEOF_LONG
-#undef SIZEOF_LONG_LONG
-#undef TYPE_BYTE
-#undef TYPE_WORD
-#undef TYPE_DWORD
-#define TYPE_UBYTE unsigned TYPE_BYTE
-#define TYPE_UWORD unsigned TYPE_WORD
-#define TYPE_UDWORD unsigned TYPE_DWORD
-#undef _M_
-#undef _A_
+#define DD_TRUE     1
+#define DD_FALSE    0
+#define NIL         0
 
 /*
  * find out the endianess of host machine
@@ -143,15 +70,6 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 # endif
 #endif
 
-#undef VERSIONSTR
-#undef VERSIONHI
-#undef VERSIONLO
-#undef VERSIONP
-
-#undef ACCEPT_SOCKLEN_T
-
-#undef STATISTIC
-
 #endif
 
-/* End of ddconfig.h */
+/* End of custom.h */
