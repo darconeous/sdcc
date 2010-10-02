3ec86bf9029248e233d4fb7feea820a9b00586ee
MaartenBrock <MaartenBrock@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2010-10-02 20:22:40 +0000

    * device/include/stdlib.h, * device/lib/_atof.c, * device/lib/_atoi.c, * device/lib/_atol.c: fixed const in prototypes
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@5998 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- device/lib/_atol.c
+++ device/lib/_atol.c
@@ -28,26 +28,25 @@
 
 long atol(char * s)
 {
-     register long rv=0; 
-     register char sign = 0;
+    register long rv=0; 
+    register char sign = 0;
 
-     /* skip till we find either a digit or '+' or '-' */
-     while (*s) {
-	if (*s <= '9' && *s >= '0')
-		break;
-	if (*s == '-' || *s == '+') 
-		break;
-	s++;
-     } 	  
-
-     sign = (*s == '-');
-     if (*s == '-' || *s == '+') s++;
-
-     while (*s && *s >= '0' && *s <= '9') {
-	rv = (rv * 10) + (*s - '0');
+    /* skip till we find either a digit or '+' or '-' */
+    while (*s) {
+        if (*s <= '9' && *s >= '0')
+            break;
+        if (*s == '-' || *s == '+') 
+            break;
         s++;
-     }
+    }
 
-     return (sign ? -rv : rv);
+    sign = (*s == '-');
+    if (*s == '-' || *s == '+') s++;
+
+    while (*s && *s >= '0' && *s <= '9') {
+        rv = (rv * 10) + (*s - '0');
+        s++;
+    }
+
+    return (sign ? -rv : rv);
 }
-
