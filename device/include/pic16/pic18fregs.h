8b024a9059144e0539839c14554baeee53a53f82
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2010-10-03 06:47:13 +0000

    * device/include/pic16/pic18fregs.h:   sdcc library license changed to GPL+LE
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@6000 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- device/include/pic16/pic18fregs.h
+++ device/include/pic16/pic18fregs.h
@@ -1,12 +1,30 @@
-/*
- * pic18fregs.h - 18F Family Device Library Header
- *
- * This file is part of the GNU PIC Library.
- *
- * January, 2004
- * The GNU PIC Library is maintained by,
- * 	Vangelis Rokas <vrokas@otenet.gr>
- */
+/*-------------------------------------------------------------------------
+   pic18fregs.h - 18F Family Device Library Header
+
+   Copyright (C) 2004, Vangelis Rokas <vrokas AT otenet.gr>
+
+   This library is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2.1, or (at your option) any
+   later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License 
+   along with this library; see the file COPYING. If not, write to the
+   Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
+   MA 02110-1301, USA.
+
+   As a special exception, if you link this library with other files,
+   some of which are compiled with SDCC, to produce an executable,
+   this library does not by itself cause the resulting executable to
+   be covered by the GNU General Public License. This exception does
+   not however invalidate any other reasons why the executable file
+   might be covered by the GNU General Public License.
+-------------------------------------------------------------------------*/
 
 #ifndef __PIC18FREGS_H__
 #define __PIC18FREGS_H__
@@ -379,4 +397,3 @@
 #define Reset()         do { __asm reset __endasm; } while(0)
 
 #endif /* __PIC18FREGS_H__ */
-
