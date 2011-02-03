515ffbfd9087a774bf07b62d1d9822406fba5ac2
borutr <borutr@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2011-02-03 20:02:05 +0000

    * support/makebin/makebin.c:   fixed bugs #3171768: makebin fails when data is at the last byte,   #3171795: If makebin read_ihx fails, it should exit with an error code
    git-svn-id: https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc@6192 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/makebin/makebin.c
+++ support/makebin/makebin.c
@@ -277,7 +277,7 @@ read_ihx (FILE *fin, BYTE *rom, int size, int *real_size)
           return 0;
         }
 
-      if (addr + nbytes >= size)
+      if (addr + nbytes > size)
         {
           fprintf (stderr, "error: size of the buffer is too small.\n");
           return 0;
@@ -467,7 +467,9 @@ main (int argc, char **argv)
       fwrite (rom, 1, (pack ? real_size : size), fout);
 
       fclose (fout);
-    }
 
-  return 0;
+      return 0;
+    }
+  else
+    return 1;
 }
